ac092e20793d26db535ad99ba0e4d8a82862d207
sarahmcdougall <smcdougall@mitre.org>
2021-10-29 10:55:09 -0400

    matt to the rescue

--- src/server/server.js
+++ src/server/server.js
@@ -1,21 +1,24 @@
-const fastify = require("fastify")({ logger: true });
-const bulkstatus = require("../services/bulkstatus.service");
-const { bulkExport } = require("../services/export.service");
+//const fastify = require("fastify")({ logger: true });
+// const bulkstatus = require("../services/bulkstatus.service");
+// const { bulkExport } = require("../services/export.service");
 const mongoUtil = require("../util/mongo");
 
 // Declare export route
-fastify.get("/$export", bulkExport);
+// fastify.get("/$export", bulkExport);
 
-// Declare bulkstatus route
-fastify.get("/bulkstatus/:clientId", bulkstatus.checkBulkStatus);
+// // Declare bulkstatus route
+// fastify.get("/bulkstatus/:clientId", bulkstatus.checkBulkStatus);
 
 // Run the server!
+
+const server = require("./app")({ logger: true });
+
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    await server.listen(3000);
     await mongoUtil.client.connect();
   } catch (err) {
-    fastify.log.error(err);
+    server.log.error(err);
     process.exit(1);
   }
 };
