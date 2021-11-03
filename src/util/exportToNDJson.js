e2aa155e1f6099b2e919b016d3412bd414f7b98d
mriley <mriley@mitre.org>
2021-11-03 11:39:38 -0400

    Update exportToNDJson.js

--- src/util/exportToNDJson.js
+++ src/util/exportToNDJson.js
@@ -1,19 +1,22 @@
-
-
+const mongoUtil = require('../util/mongo');
+const supportedResources = require('../util/supportedResources');
+const fs = require('fs');
 
 const exportToNDJson = async clientId => {
   let dirpath = '/tmp/';
   await fs.promises.mkdir(dirpath, { recursive: true });
-  supportedResources.map(async resourceType => {
+  supportedResources.foreach(async resourceType => {
     //make dir for the resource
     dirpath = dirpath + resourceType.toString();
     await fs.promises.mkdir(dirpath, { recursive: true });
-    //get a collection
-    const filename = dirpath + resourceType + clientId + '.json';
-    db.resourceType.foreach(function (err, myResource) {
+    let collections = db.collection(resourceType);
+    const filename = dirpath + resourceType + clientId + '.ndjson';
+    collections.foreach(function (err, myResource) {
       let result = JSON.parse(JSON.stringify(myResource));
       fs.appendFileSync(dirpath, (++lineCount === 1 ? '' : '\r\n') + JSON.stringify(result));
     });
     fs.writeFileSync(filename, result);
   });
 };
+
+module.exports = { exportToNDJson };
