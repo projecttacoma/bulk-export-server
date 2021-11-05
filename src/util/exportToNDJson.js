d426679827194c67ca3f75527b0fdc6d787d24c0
mriley <mriley@mitre.org>
2021-11-05 11:09:11 -0400

    MInor updates

--- src/util/exportToNDJson.js
+++ src/util/exportToNDJson.js
@@ -2,40 +2,34 @@ const { db } = require('./mongo');
 const supportedResources = require('../util/supportedResources');
 const fs = require('fs');
 
+/**
+ * Exports the list of resources included in the _type member of the request object to NDJson
+ * if the _type member doesn't exist it willl simply export everything included in the supportedResources list
+ * @param {Object} request http request object
+ * @param {*} clientId the response object
+ */
 const exportToNDJson = async (clientId, request) => {
   let dirpath = './tmp/';
   await fs.promises.mkdir(dirpath, { recursive: true });
-  //create list of requested types if request.query._types param exists
-  if (request.query._types) {
-    let lineCount = 0;
-    const requestTypes = request.query._type.split(','); //this is the list types to export
-    requestTypes.foreach(type => {
-      dirpath = dirpath + type.toString();
-      let collections = db.collection(type);
-      const filename = dirpath + '/' + type + clientId + '.ndjson';
-      collections.foreach(function (doc) {
-        let result = JSON.parse(JSON.stringify(doc));
-        fs.appendFileSync(dirpath, (++lineCount === 1 ? '' : '\r\n') + JSON.stringify(result));
-      });
-      fs.writeFileSync(filename);
-    });
+  let requestTypes; 
+  if (request.query._type) {
+    requestTypes = request.query._type.split(','); //this is the list types to export
   } else {
-    let lineCount = 0;
-    supportedResources.foreach(async resourceType => {
-      //make dir for the resource
-      dirpath = dirpath + resourceType.toString();
-      await fs.promises.mkdir(dirpath, { recursive: true });
-      let collections = db.collection(resourceType);
-      const filename = dirpath + '/' + resourceType + clientId + '.ndjson';
-      collections.foreach(function (myResource) {
-        fs.appendFileSync(
-          dirpath,
-          (++lineCount === 1 ? '' : '\r\n') + JSON.stringify(JSON.parse(JSON.stringify(myResource)))
-        );
-      });
-      fs.writeFileSync(filename);
-    });
+    //create list of requested types if request.query._type param doesn't exist
+    requestTypes.push(supportedResources);
   }
+
+  let lineCount = 0;
+  requestTypes.foreach(type => {
+    dirpath = dirpath + type.toString();
+    let collections = db.collection(type);
+    const filename = dirpath + '/' + type + clientId + '.ndjson';
+    collections.foreach(function (doc) {
+      let result = JSON.parse(JSON.stringify(doc));
+      fs.appendFileSync(dirpath, (++lineCount === 1 ? '' : '\r\n') + JSON.stringify(result));
+    });
+    fs.writeFileSync(filename);
+  });
 };
 
 module.exports = { exportToNDJson };
