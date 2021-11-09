4482db14a5c315c95958f37977c9ec61ceb07ee2
MegRiley <53576881+MegRiley@users.noreply.github.com>
2021-11-09 15:52:06 -0500

    Export to ndjson (#5)
    * Create exportToNDJson.js
    
    * Update exportToNDJson.js
    
    * Update exportToNDJson.js
    
    * Added unit tests for file creation
    
    * Update exportToNDJsonTest.js
    
    * Prettier updates
    
    * MInor updates
    
    * Update exportToNDJson.js
    
    * Update populateTestData.js
    
    * Update exportToNDJson.test.js
    
    * Update exportToNDJson.test.js
    
    * Update exportToNDJson.test.js
    
    * Added connect to the create data for test for function
    
    * Made changes to address some pr comments
    
    * Update exportToNDJson.js
    
    * Update exportToNDJson.js
    
    * Minor Unit test changes
    
    * Update exportToNDJson.js
    
    * Update exportToNDJson.js
    
    * Minor tweaks
    
    * Update exportToNDJson.js
    
    * Update exportToNDJson.js
    
    * Update exportToNDJson.js
    
    * Apply suggestions from code review
    
    Co-authored-by: Matthew Gramigna <mgramigna@mitre.org>
    
    * Updated the tests after Matt's feedback
    
    * Reverting the accidental commit
    
    * Update exportToNDJson.js
    
    * Update .gitignore
    
    * Update exportToNDJson.js
    
    * Update exportToNDJson.js
    
    * Update exportToNDJson.test.js
    
    * Update exportToNDJson.js
    
    * Update exportToNDJson.js
    
    * Applied Lauren's suggestions
    
    Co-authored-by: lmd59 <laurend@mitre.org>
    
    * Update exportToNDJson.js
    
    * Update exportToNDJson.js
    
    Co-authored-by: Matthew Gramigna <mgramigna@mitre.org>
    Co-authored-by: lmd59 <laurend@mitre.org>

---
+++ src/util/exportToNDJson.js
@@ -0,0 +1,53 @@
+const { db } = require('./mongo');
+const supportedResources = require('../util/supportedResources');
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Exports the list of resources included in the _type member of the request object to NDJson
+ * if the _type member doesn't exist it will simply export everything included in the supportedResources list
+ * @param {Object} request http request object
+ * @param {*} clientId  an id to add to the file name so the client making the request can be tracked
+ */
+const exportToNDJson = async (clientId, request) => {
+  let dirpath = './tmp/';
+  fs.mkdirSync(dirpath, { recursive: true });
+  let requestTypes = [];
+  if (request.query._type) {
+    requestTypes = request.query._type.split(','); //this is the list types to export
+  } else {
+    //create list of requested types if request.query._type param doesn't exist
+    requestTypes.push(...supportedResources);
+  }
+  let docs = requestTypes.map(async element => {
+    return getDocuments(db, element);
+  });
+  docs = await Promise.all(docs);
+  docs.forEach(doc => {
+    writeToFile(doc.document, doc.collectionName, clientId);
+  });
+};
+
+const getDocuments = async (db, collectionName) => {
+  const query = {};
+  let doc = await db.collection(collectionName.toString()).find(query).toArray();
+  return { document: doc, collectionName: collectionName.toString() };
+};
+
+const writeToFile = function (doc, type, clientId) {
+  let dirpath = './tmp/' + clientId;
+  fs.mkdirSync(dirpath, { recursive: true });
+  const filename = path.join(dirpath, `${type}.ndjson`);
+
+  let lineCount = 0;
+
+  if (Object.keys(doc).length > 0) {
+    const stream = fs.createWriteStream(filename, { flags: 'a' });
+    doc.forEach(function (doc) {
+      stream.write((++lineCount === 1 ? '' : '\r\n') + JSON.stringify(doc));
+    });
+    stream.end();
+  } else return;
+};
+
+module.exports = { exportToNDJson };
---
+++ test/exportToNDJson.test.js
@@ -0,0 +1,41 @@
+const build = require('../src/server/app');
+const { exportToNDJson } = require('../src/util/exportToNDJson');
+const { cleanUpDb, createTestResourceWithConnect } = require('./populateTestData');
+const testPatient = require('./fixtures/testPatient.json');
+const app = build();
+const fs = require('fs');
+const mockRequestWithType = {
+  query: {
+    _type: 'Patient'
+  }
+};
+const mockRequestWithoutType = {
+  id: 'mockRequestWithoutType',
+  query: {}
+};
+
+const expectedFileName = './tmp/123456/Patient.ndjson';
+const clientId = '123456';
+describe('check export logic', () => {
+  beforeAll(async () => {
+    await createTestResourceWithConnect(testPatient, 'Patient');
+    console.log('created test resource');
+  });
+
+  beforeEach(async () => {
+    await app.ready();
+  });
+
+  test('Expect folder created and export successful when _type  parameter is present', async () => {
+    await exportToNDJson(clientId, mockRequestWithType);
+    expect(fs.existsSync(expectedFileName)).toBe(true);
+  });
+  test('Expect folder created and export successful when _type  parameter is not present', async () => {
+    await exportToNDJson(clientId, mockRequestWithoutType);
+
+    expect(fs.existsSync(expectedFileName)).toBe(true);
+  });
+  afterAll(async () => {
+    await cleanUpDb();
+  });
+});
