9085f9cc6a9fb076f7033104326733ff4579242e
mriley <mriley@mitre.org>
2021-11-09 12:06:41 -0500

    Update exportToNDJson.js

--- src/util/exportToNDJson.js
+++ src/util/exportToNDJson.js
@@ -1,7 +1,6 @@
 const { db } = require('./mongo');
 const supportedResources = require('../util/supportedResources');
 const fs = require('fs');
-const { type } = require('os');
 
 /**
  * Exports the list of resources included in the _type member of the request object to NDJson
@@ -17,23 +16,22 @@ const exportToNDJson = async (clientId, request) => {
     requestTypes = request.query._type.split(','); //this is the list types to export
   } else {
     //create list of requested types if request.query._type param doesn't exist
-    requestTypes.push(supportedResources);
+    requestTypes.push(...supportedResources);
   }
   let docs = requestTypes.map(async element => {
     return getDocuments(db, element);
   });
   docs = await Promise.all(docs);
   let p = docs.map(async doc => {
-    let type = requestTypes.pop();
     return writeToFile(doc.doc, doc.collectionName, clientId);
   });
-  await Promise.all(requestTypes);
+  await Promise.all(p);
 };
 
 const getDocuments = async (db, collectionName) => {
   const query = {};
-  let doc =  await db.collection(collectionName.toString()).find(query).toArray();
-   return {document: doc, collectionName: collectionName.toString()};
+  let doc = await db.collection(collectionName.toString()).find(query).toArray();
+  return { document: doc, collectionName: collectionName.toString() };
 };
 
 const writeToFile = async (doc, type, clientId) => {
@@ -45,12 +43,16 @@ const writeToFile = async (doc, type, clientId) => {
   fs.open(filename, 'w', function (err) {
     if (err) throw err;
   });
-  var stream = fs.createWriteStream(filename, { flags: 'a' });
-  doc.forEach(function (doc) {
-    let result = JSON.parse(JSON.stringify(doc));
-    stream.write((++lineCount === 1 ? '' : '\r\n') + JSON.stringify(result));
-  });
-  stream.end();
+  if (doc) {
+    doc.forEach(function (doc) {
+      var stream = fs.createWriteStream(filename, { flags: 'a' });
+
+      let result = JSON.parse(JSON.stringify(doc));
+      stream.write((++lineCount === 1 ? '' : '\r\n') + JSON.stringify(result));
+    });
+
+    stream.end();
+  } else return;
 };
 
 module.exports = { exportToNDJson };
