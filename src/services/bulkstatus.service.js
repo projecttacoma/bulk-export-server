e1a5b01a02c9c341fe6f95d4833628fcf81e8f9a
natjoe4 <njjones@mitre.org>
2021-11-18 09:44:48 -0500

    documentation

--- src/services/bulkstatus.service.js
+++ src/services/bulkstatus.service.js
@@ -24,6 +24,7 @@ async function checkBulkStatus(request, reply) {
       transactionTime: new Date(),
       requiresAccessToken: false,
       outcome: responseData,
+      // When we eventually catch warnings, this will add them to the response object
       ...(bulkStatus.warnings.length === 0
         ? undefined
         : {
