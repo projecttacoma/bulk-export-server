9a1d4a97168ff64265b05e48cde30187e664783c
natjoe4 <35576465+natjoe4@users.noreply.github.com>
2021-11-19 15:58:01 -0500

    Update src/util/errorUtils.js
    Co-authored-by: Sarah McDougall <87094479+sarahmcdougall@users.noreply.github.com>

--- src/util/errorUtils.js
+++ src/util/errorUtils.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 /**
  * Takes in error information and create an OperationOutcome to return to the user
  * @param {*} message a message to be displayed inside the OperationOutcome
- * @param {*} param1 an object containing the issueCode and severity to be copited into the OperationOutcome
+ * @param {*} param1 an object containing the issueCode and severity to be copied into the OperationOutcome
  * @returns An OperationOutcome to return to the user with information about the failure
  */
 
