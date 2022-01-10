17e722c374dd6b97dbcdec67557ca3f1bd551489
natjoe4 <35576465+natjoe4@users.noreply.github.com>
2022-01-10 16:06:34 -0500

    EXM347 ignore (#14)
    * ignored EXM347 bundle
    
    * Added comments
    
    Co-authored-by: natjoe4 <njjones@mitre.org>

--- src/scripts/getConnectathonBundles.js
+++ src/scripts/getConnectathonBundles.js
@@ -4,8 +4,8 @@ const mongoUtil = require('../util/mongo');
 const { createResource } = require('../util/mongo.controller');
 
 const connectathonPath = path.resolve(path.join(__dirname, '../../connectathon/fhir401/bundles/measure/'));
-// bundles that are not the latest available version
-const IGNORED_BUNDLES = ['EXM347-3.2.000-bundle.json'];
+// bundles that are not the latest available version or contain reference errors
+const IGNORED_BUNDLES = ['EXM347-3.2.000-bundle.json', 'EXM347-4.3.000-bundle.json'];
 // files containing EXM bundles of interest from connectathon repo
 const bundleFiles = [];
 
