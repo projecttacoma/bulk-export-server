b687a0bbc5ae3ab0b333a95ba8b991c0dc0e8ffe
sarahmcdougall <smcdougall@mitre.org>
2022-05-04 14:36:43 -0400

    add basic group read/search operations

--- src/server/app.js
+++ src/server/app.js
@@ -3,6 +3,7 @@ const fastify = require('fastify');
 const { bulkExport, patientBulkExport, groupBulkExport } = require('../services/export.service');
 const { checkBulkStatus } = require('../services/bulkstatus.service');
 const { returnNDJsonContent } = require('../services/ndjson.service');
+const { groupSearchById, groupSearch, groupCreate, groupUpdate } = require('../services/group.service');
 
 function build(opts = {}) {
   const app = fastify(opts);
@@ -11,6 +12,10 @@ function build(opts = {}) {
   app.get('/Group/:groupId/$export', groupBulkExport);
   app.get('/bulkstatus/:clientId', checkBulkStatus);
   app.get('/:clientId/:fileName', returnNDJsonContent);
+  app.get('/Group/:groupId', groupSearchById);
+  app.get('/Group/', groupSearch);
+  app.post('/Group/', groupCreate);
+  app.put('/Group/:groupId', groupUpdate);
   return app;
 }
 
---
+++ src/services/group.service.js
@@ -0,0 +1,67 @@
+const {
+  findResourceById,
+  findResourcesWithQuery,
+  createResource,
+  updateResource
+} = require('../util/mongo.controller');
+const { v4: uuidv4 } = require('uuid');
+
+/**
+ * Result of sending a GET request to [base]/Group/[id].
+ * Searches for a Group resource with the passed in id
+ * @param {Object} request the request object passed in by the user
+ * @param {Object} reply the response object
+ */
+const groupSearchById = async (request, reply) => {
+  const result = await findResourceById(request.params.groupId, 'Group');
+  if (!result) {
+    reply.code(404).send(new Error(`The requested group ${request.params.groupId} was not found.`));
+  }
+  return result;
+};
+
+/**
+ * Result of sending a GET request to [base]/Group to find all available Groups.
+ * Searches for a Group resource with the passed in id
+ * @param {Object} request the request object passed in by the user
+ * @param {Object} reply the response object
+ */
+const groupSearch = async (request, reply) => {
+  const result = await findResourcesWithQuery({}, 'Group');
+  if (!result.length > 0) {
+    reply.code(404).send(new Error('No Group resources were found on the server'));
+  }
+  return result;
+};
+
+/**
+ * Creates an object and generates an id for it regardless of the id passed in
+ * @param {Object} request the request object passed in by the user
+ * @param {Object} reply the response object
+ */
+const groupCreate = async request => {
+  const data = request.body;
+  data['id'] = uuidv4();
+  return createResource(data, 'Group');
+};
+
+/**
+ * Updates the Group resource with the passed in id or creates a new document if
+ * no document with passed id is found
+ * @param {Object} request the request object passed in by the user
+ * @param {Object} reply the response object
+ */
+const groupUpdate = async (request, reply) => {
+  const data = request.body;
+  if (data.id !== request.params.groupId) {
+    reply.code(300).send(new Error('Argument id must match request body id for PUT request'));
+  }
+  return updateResource(request.params.groupId, data, 'Group');
+};
+
+module.exports = {
+  groupSearchById,
+  groupSearch,
+  groupCreate,
+  groupUpdate
+};
