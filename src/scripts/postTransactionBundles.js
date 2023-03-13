820ccf271e1d519b755f351dee5d3bb36a408df9
sarahmcdougall <smcdougall@mitre.org>
2023-03-13 15:42:25 -0400

    prettier

--- src/scripts/postTransactionBundles.js
+++ src/scripts/postTransactionBundles.js
@@ -21,26 +21,30 @@ async function main() {
   // store uploaded patientIds to be added as members to FHIR Group
   const patientRegEx = new RegExp('Patient/[^/]*');
 
-  directoryFiles.filter(file => file.startsWith('practitioner') || file.startsWith('hospital')).map(async file => {
-    await axios.post(
-      `http://${process.env.HOST}:${process.env.PORT}/`,
-      JSON.parse(fs.readFileSync(path.join(bundlePath, file), 'utf8')),
-      { headers: { 'Content-Type': 'application/json+fhir' } }
-    );
-  });
+  directoryFiles
+    .filter(file => file.startsWith('practitioner') || file.startsWith('hospital'))
+    .map(async file => {
+      await axios.post(
+        `http://${process.env.HOST}:${process.env.PORT}/`,
+        JSON.parse(fs.readFileSync(path.join(bundlePath, file), 'utf8')),
+        { headers: { 'Content-Type': 'application/json+fhir' } }
+      );
+    });
 
-  const patientIdsArray = directoryFiles.filter(file => !file.startsWith('practitioner') && !file.startsWith('hospital')).map(async file => {
-    const { data } = await axios.post(
-      `http://${process.env.HOST}:${process.env.PORT}/`,
-      JSON.parse(fs.readFileSync(path.join(bundlePath, file), 'utf8')),
-      { headers: { 'Content-Type': 'application/json+fhir' } }
-    );
+  const patientIdsArray = directoryFiles
+    .filter(file => !file.startsWith('practitioner') && !file.startsWith('hospital'))
+    .map(async file => {
+      const { data } = await axios.post(
+        `http://${process.env.HOST}:${process.env.PORT}/`,
+        JSON.parse(fs.readFileSync(path.join(bundlePath, file), 'utf8')),
+        { headers: { 'Content-Type': 'application/json+fhir' } }
+      );
 
-    const location = data.entry.find(e => e.response.location.startsWith('/Patient'));
-    if (patientRegEx.test(location?.response.location)) {
-      return location.response.location.replace('/Patient/', '');
-    }
-  });
+      const location = data.entry.find(e => e.response.location.startsWith('/Patient'));
+      if (patientRegEx.test(location?.response.location)) {
+        return location.response.location.replace('/Patient/', '');
+      }
+    });
   const patientIds = await Promise.all(patientIdsArray);
   const success = await createPatientGroupsPerMeasure(groupId, patientIds);
   if (success) {
