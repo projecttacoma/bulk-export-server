a65d1237f21ff3fef79587a7c04f3340b2e029ac
sarahmcdougall <smcdougall@mitre.org>
2023-03-14 12:43:42 -0400

    update upload script for incremental group creation

--- src/scripts/postTransactionBundles.js
+++ src/scripts/postTransactionBundles.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 const { createPatientGroupsPerMeasure } = require('../util/groupUtils');
 const mongoUtil = require('../util/mongo');
 
+// for each group, include the patients that are members of the previous group
+// (ex. creates a group of size 10, a group of size 100, ..., a group of size 20,000)
+const GROUP_SIZES = [10, 90, 900]; //, 9000, 1000];
+
 /**
  * Uploads all transaction bundles from the specified directory
  * into the database. Creates a group resource containing
@@ -18,44 +22,51 @@ async function main() {
   await mongoUtil.client.connect();
   console.log('Connected successfully to server');
 
-  const bundlePath = path.resolve(process.argv[2]);
-  const groupId = process.argv[3];
-  const directoryFiles = fs.readdirSync(bundlePath);
-  // store uploaded patientIds to be added as members to FHIR Group
+  const bundleDir = path.resolve(process.argv[2]);
+
   const patientRegEx = new RegExp('Patient/[^/]*');
+  // store uploaded patientIds to be added as members to FHIR Group (across all patients uploaded so far)
+  const allPatientIds = [];
 
-  // upload practitioner/hospital batch bundles first, if present
-  directoryFiles
-    .filter(file => file.startsWith('practitioner') || file.startsWith('hospital'))
-    .map(async file => {
-      await axios.post(
-        `http://${process.env.HOST}:${process.env.PORT}/`,
-        JSON.parse(fs.readFileSync(path.join(bundlePath, file), 'utf8')),
-        { headers: { 'Content-Type': 'application/json+fhir' } }
-      );
-    });
+  for (const groupSize of GROUP_SIZES) {
+    const directoryPath = path.join(bundleDir, `cms122-${groupSize}-patients`, 'fhir');
+    const directoryFiles = fs.readdirSync(directoryPath);
 
-  const patientIdsArray = directoryFiles
-    .filter(file => !file.startsWith('practitioner') && !file.startsWith('hospital'))
-    .map(async file => {
-      const { data } = await axios.post(
-        `http://${process.env.HOST}:${process.env.PORT}/`,
-        JSON.parse(fs.readFileSync(path.join(bundlePath, file), 'utf8')),
-        { headers: { 'Content-Type': 'application/json+fhir' } }
-      );
+    // upload practitioner/hospital batch bundles first, if present
+    directoryFiles
+      .filter(file => file.startsWith('practitioner') || file.startsWith('hospital'))
+      .map(async file => {
+        await axios.post(
+          `http://${process.env.HOST}:${process.env.PORT}/`,
+          JSON.parse(fs.readFileSync(path.join(directoryPath, file), 'utf8')),
+          { headers: { 'Content-Type': 'application/json+fhir' } }
+        );
+      });
 
-      const location = data.entry.find(e => e.response.location.startsWith('/Patient'));
-      if (patientRegEx.test(location?.response.location)) {
-        return location.response.location.replace('/Patient/', '');
-      }
-    });
-  const patientIds = await Promise.all(patientIdsArray);
-  const success = await createPatientGroupsPerMeasure(groupId, patientIds);
-  if (success) {
-    return `Group ${groupId}-patients successfully created`;
-  } else {
-    return 'Group creation failed';
+    const patientIdsArray = directoryFiles
+      .filter(file => !file.startsWith('practitioner') && !file.startsWith('hospital'))
+      .map(async file => {
+        const { data } = await axios.post(
+          `http://${process.env.HOST}:${process.env.PORT}/`,
+          JSON.parse(fs.readFileSync(path.join(directoryPath, file), 'utf8')),
+          { headers: { 'Content-Type': 'application/json+fhir' } }
+        );
+
+        const location = data.entry.find(e => e.response.location.startsWith('/Patient'));
+        if (patientRegEx.test(location?.response.location)) {
+          return location.response.location.replace('/Patient/', '');
+        }
+      });
+    const patientIds = await Promise.all(patientIdsArray);
+    allPatientIds.push(...patientIds);
+    const success = await createPatientGroupsPerMeasure(allPatientIds.length, allPatientIds);
+    if (success) {
+      console.log(`Group cms122-${allPatientIds.length}-patients successfully created`);
+    } else {
+      console.log('Group creation failed');
+    }
   }
+  return 'Group upload finished';
 }
 
 main()
