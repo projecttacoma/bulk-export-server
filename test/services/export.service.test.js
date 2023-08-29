7e99625e594d428f18f1d37bbca24bdafe4dd5de
sarahmcdougall <smcdougall@mitre.org>
2023-08-29 10:29:39 -0400

    add group-level failure tests, add _typeFilter invalid type tests

--- test/services/export.service.test.js
+++ test/services/export.service.test.js
@@ -165,6 +165,19 @@ describe('Check barebones bulk export logic (failure)', () => {
       });
   });
 
+  test('throws 400 error when _typeFilter param is supplied with an invalid/unsupported resource type', async () => {
+    await supertest(app.server)
+      .get('/$export?_typeFilter=invalid?status=active')
+      .expect(400)
+      .then(response => {
+        expect(response.body.resourceType).toEqual('OperationOutcome');
+        expect(response.body.issue[0].code).toEqual(400);
+        expect(response.body.issue[0].details.text).toEqual(
+          'The following resourceTypes are not supported for _typeFilter param for $export: invalid.'
+        );
+      });
+  });
+
   afterEach(async () => {
     await cleanUpDb();
   });
@@ -233,6 +246,108 @@ describe('Check patient-level export logic (failure)', () => {
       .expect(400)
       .then(async () => {
         expect(await db.collection('bulkExportStatuses').countDocuments({})).toEqual(numEntries);
+      });
+  });
+
+  test('throws 400 error when _typeFilter param is supplied with an invalid/unsupported resource type', async () => {
+    await supertest(app.server)
+      .get('/Patient/$export?_typeFilter=invalid?status=active')
+      .expect(400)
+      .then(response => {
+        expect(response.body.resourceType).toEqual('OperationOutcome');
+        expect(response.body.issue[0].code).toEqual(400);
+        expect(response.body.issue[0].details.text).toEqual(
+          'The following resourceTypes are not supported for _typeFilter param for $export: invalid.'
+        );
+      });
+  });
+
+  afterEach(async () => {
+    await cleanUpDb();
+  });
+});
+
+describe('Check group-level export logic (failure)', () => {
+  const GROUP_ID = 'testGroup';
+
+  beforeEach(async () => {
+    await bulkStatusSetup();
+    await createTestResource(testPatient, 'Patient');
+    await createTestResource(testEncounter, 'Encounter');
+    await createTestResource(testGroup, 'Group');
+    await app.ready();
+  });
+  test('check 400 returned for invalid outputFormat', async () => {
+    await supertest(app.server)
+      .get(`/Group/${GROUP_ID}/$export?_outputFormat=invalid`)
+      .expect(400)
+      .then(response => {
+        expect(JSON.parse(response.text).message).toEqual(
+          'The following output format is not supported for _outputFormat param for $export: invalid'
+        );
+      });
+  });
+
+  test('check 400 returned for invalid type (not a supported resource)', async () => {
+    await supertest(app.server)
+      .get(`/Group/${GROUP_ID}/$export?_type=invalid`)
+      .expect(400)
+      .then(response => {
+        expect(response.body.resourceType).toEqual('OperationOutcome');
+        expect(response.body.issue[0].code).toEqual(400);
+        expect(response.body.issue[0].details.text).toEqual(
+          'The following resourceTypes are not supported for _type param for $export: invalid.'
+        );
+      });
+  });
+
+  test('check 400 returned when no resource types in _type param are included in patient compartment', async () => {
+    // Parameters is a supported resource that does not appear in the patient compartment
+    await supertest(app.server)
+      .get(`/Group/${GROUP_ID}/$export?_type=Parameters`)
+      .expect(400)
+      .then(response => {
+        expect(response.body.resourceType).toEqual('OperationOutcome');
+        expect(response.body.issue[0].code).toEqual(400);
+        expect(response.body.issue[0].details.text).toEqual(
+          'None of the provided resource types are permitted for Patient/Group export.'
+        );
+      });
+  });
+
+  test('check 400 returned for unrecognized param', async () => {
+    await supertest(app.server)
+      .get(`/Group/${GROUP_ID}/$export?_unrecognizedparam=invalid`)
+      .expect(400)
+      .then(response => {
+        expect(response.body.resourceType).toEqual('OperationOutcome');
+        expect(response.body.issue[0].code).toEqual(400);
+        expect(response.body.issue[0].details.text).toEqual(
+          'The following parameters are unrecognized by the server: _unrecognizedparam.'
+        );
+      });
+  });
+
+  test('DB is not populated when unrecognized param is in request', async () => {
+    const numEntries = await db.collection('bulkExportStatuses').countDocuments({});
+    await supertest(app.server)
+      .get(`/Group/${GROUP_ID}/$export?_unrecognizedparam=invalid`)
+      .expect(400)
+      .then(async () => {
+        expect(await db.collection('bulkExportStatuses').countDocuments({})).toEqual(numEntries);
+      });
+  });
+
+  test('throws 400 error when _typeFilter param is supplied with an invalid/unsupported resource type', async () => {
+    await supertest(app.server)
+      .get(`/Group/${GROUP_ID}/$export?_typeFilter=invalid?status=active`)
+      .expect(400)
+      .then(response => {
+        expect(response.body.resourceType).toEqual('OperationOutcome');
+        expect(response.body.issue[0].code).toEqual(400);
+        expect(response.body.issue[0].details.text).toEqual(
+          'The following resourceTypes are not supported for _typeFilter param for $export: invalid.'
+        );
       });
   });
 
