d4868eeadd40d2f786cf8b54c4babf405b3c512f
Elsa Perelli <eperelli@mitre.org>
2024-05-18 10:08:46 -0400

    Capability statement (#42)
    * Initial code for adding /metadata endpoint for capability statement
    
    * Remove fs
    
    * removed security section
    
    ---------
    
    Co-authored-by: Chris Hossenlopp <hossenlopp@mitre.org>

---
+++ src/services/metadata.service.js
@@ -0,0 +1,10 @@
+const capabilityStatementResource = require('../config/capabilityStatementResource.json');
+
+const generateCapabilityStatement = async (request, reply) => {
+  request.log.info(`Metadata.generateCapabilityStatement`);
+
+  reply.code(200);
+  reply.send(capabilityStatementResource);
+};
+
+module.exports = { generateCapabilityStatement };
